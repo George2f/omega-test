0b912ed0bd326dcbfec23b13e92e6d0dbe69e769
George2f <juraj.pejnovic@2front.co>
2024-06-14 01:25:49 +0200

    build(omega-test): update gitignore

--- src/model/Article/hooks/useGetArticles.ts
+++ src/model/Article/hooks/useGetArticles.ts
@@ -1,12 +1,7 @@
 import { useQuery } from "@tanstack/react-query"
 import getArticles from "../api/getArticles"
 
-export default function useGetArticles({
-  contractId,
-}: {
-  contractId: number
-  articleId: number
-}) {
+export default function useGetArticles({ contractId }: { contractId: number }) {
   const { data, error, isPending } = useQuery({
     queryKey: ["article", contractId],
     queryFn: () => getArticles({ contractId }),
